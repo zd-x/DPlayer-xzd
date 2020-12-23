7db416172e3a7cf65ba44d2cbb2654f51a452a05
xiaozhengdong <xiaozhengdong@cmiot.chinamobile.com>
2020-12-23 18:36:39 +0800

    fix: video标签contextmenu原生事件阻止有漏掉的情况

--- src/js/contextmenu.js
+++ src/js/contextmenu.js
@@ -13,13 +13,13 @@ class ContextMenu {
         });
 
         this.player.container.addEventListener('contextmenu', (e) => {
+            const event = e || window.event;
+            event.preventDefault();
+
             if (this.shown) {
                 this.hide();
                 return;
             }
-
-            const event = e || window.event;
-            event.preventDefault();
 
             const clientRect = this.player.container.getBoundingClientRect();
             this.show(event.clientX - clientRect.left, event.clientY - clientRect.top);
